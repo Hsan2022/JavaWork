87b3f01b572349ef09e81b69d2613dcc70bb48d7
HSOrduno <103794936+Hsan2022@users.noreply.github.com>
2024-05-09 07:32:32 -0600

    Added missing files.

--- Java Classwork/src/main/java/org/example/pa_303_2_2/Java_Stdin_and_Stdout_I.java
+++ Java Classwork/src/main/java/org/example/pa_303_2_2/Java_Stdin_and_Stdout_I.java
@@ -1,25 +1,27 @@
 package org.example.pa_303_2_2;
 
+import java.util.Scanner;
+
 public class Java_Stdin_and_Stdout_I {
 
     public static void main(String[] args) {
 
-        //Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-//        System.out.println("Enter number:");
-//        int i = scan.nextInt();
-//        System.out.println("Int: " + i);
-//
-//        System.out.println("Enter double:");
-//        Double d = scan.nextDouble();
-//        System.out.println("Double: " + d);
-//
-//        System.out.println("Enter string:");
-//        scan.nextLine();
-//        String s = scan.nextLine();
-//        System.out.println("String: " + s);
+        System.out.println("Enter number:");
+        int i = scan.nextInt();
+        System.out.println("Int: " + i);
 
-        //scan.close();
+        System.out.println("Enter double:");
+        Double d = scan.nextDouble();
+        System.out.println("Double: " + d);
+
+        System.out.println("Enter string:");
+        scan.nextLine();
+        String s = scan.nextLine();
+        System.out.println("String: " + s);
+
+        scan.close();
     }
 }
 
--- Java Classwork/src/main/java/org/example/pa_303_2_2/Java_Stdin_and_Stdout_II.java
+++ Java Classwork/src/main/java/org/example/pa_303_2_2/Java_Stdin_and_Stdout_II.java
@@ -1,5 +1,21 @@
 package org.example.pa_303_2_2;
 
+import java.util.Scanner;
+
+import static java.lang.System.*;
+
+
 public class Java_Stdin_and_Stdout_II {
 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        Double d = scan.nextDouble();
+        String s = scan.nextLine();
+        s = scan.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
 }
